af85fbf746dd0cb8ee1092f39398fa2a8c8e670b
chansreeniv <chansreeniv@yahoo.com>
2021-03-18 07:50:42 +0530

    dynamic content rendered

--- express_basics/app.js
+++ express_basics/app.js
@@ -5,19 +5,19 @@ const express = require('express');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin',adminRoutes);
+app.use('/admin',adminData.routes);
 
 app.use(shopRouter);
 
 app.use((req, res, next) => {
     // res.status(404).send('<h1>Page Not Found</h1>');
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000);
--- express_basics/routes/admin.js
+++ express_basics/routes/admin.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const product = [];
+
 const rootDir = require('../util/path');
 
 router.get('/add-product',(req, res, next) => {
@@ -11,8 +13,12 @@ router.get('/add-product',(req, res, next) => {
 });
 
 router.post('/add-product', (req, res) =>{
-    console.log(req.body);
+    // console.log(req.body);
+    product.push({title: req.body.title});
     res.redirect('/');
 })
 
-module.exports = router;
+// module.exports = router;
+
+exports.routes = router;
+exports.product = product;
